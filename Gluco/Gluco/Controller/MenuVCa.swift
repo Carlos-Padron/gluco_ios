0eb95c75d65ec5d9fe0f13e1318bdd54f08a6e74
Carlos Padron <c_0509@hotmail.com>
2019-09-20 00:24:59 -0500

    Se agregó los elementos de la barra lateral

--- Gluco/Gluco/Controller/MenuVCa.swift
+++ Gluco/Gluco/Controller/MenuVCa.swift
@@ -8,11 +8,51 @@
 
 import UIKit
 
-class MenuVC: UIViewController {
+class MenuVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //Outlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var user: String = "paciente"
     
     override func viewDidLoad() {
-        self.revealViewController().rearViewRevealWidth = (self.view.frame.size.width - 40)
+        self.revealViewController().rearViewRevealWidth = (self.view.frame.size.width - 150)
+        tableView.dataSource = self
+        tableView.delegate = self
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if(user == "paciente"){//paciente
+            return DataService.instance.getUserMenu().count
+        }else if(user == "doc"){
+            return DataService.instance.getDoctorMenu().count
+        }else {
+            return 0
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "menuCell", for: indexPath) as? menuCell{
+            if(user == "paciente"){//Paciente
+                let menu = DataService.instance.getUserMenu()[indexPath.row]
+                cell.updateMenu(Menu: menu)
+                cell.layer.backgroundColor = UIColor.clear.cgColor
+            }else if(user == "doc"){
+                let menu = DataService.instance.getDoctorMenu()[indexPath.row]
+                cell.updateMenu(Menu: menu)
+                cell.layer.backgroundColor = UIColor.clear.cgColor
+            }
+            return cell
+        }else {
+            return menuCell()
+        }
+    }///
+    
     
 
 }
