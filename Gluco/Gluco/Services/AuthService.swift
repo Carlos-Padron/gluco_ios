ce821f09e4f9463abe55a12a1f25b873ceaba783
Carlos Padron <c_0509@hotmail.com>
2019-10-19 12:32:08 -0500

    comit

---
+++ Gluco/Gluco/Services/AuthService.swift
@@ -0,0 +1,9 @@
+//
+//  AuthService.swift
+//  Gluco
+//
+//  Created by Carlos Padrón on 9/30/19.
+//  Copyright © 2019 Carlos Padrón. All rights reserved.
+//
+
+import Foundation
---
+++ Gluco/Gluco/Services/Variables.swift
@@ -0,0 +1,9 @@
+//
+//  Variables.swift
+//  Gluco
+//
+//  Created by Carlos Padrón on 10/16/19.
+//  Copyright © 2019 Carlos Padrón. All rights reserved.
+//
+
+import Foundation
